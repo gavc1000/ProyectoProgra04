e4ae4198bd74686aaad73282e73f4758bc5d13d7
Jorge Isaac <jorgeisaacjimenezb@hotmail.com>
2017-11-18 17:37:10 -0600

    :v

--- ProyectoProgra04/ProyectoProgra04/GUI/LogIn.Designer.cs
+++ ProyectoProgra04/ProyectoProgra04/GUI/LogIn.Designer.cs
@@ -28,11 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(71, 184);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(225, 22);
+            this.txtuser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Font = new System.Drawing.Font("Californian FB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Los Cositos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label2.Font = new System.Drawing.Font("Californian FB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Banco";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(71, 258);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(225, 22);
+            this.txtpass.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(68, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label4.Location = new System.Drawing.Point(68, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = global::ProyectoProgra04.Properties.Resources.Untitled;
+            this.ClientSize = new System.Drawing.Size(382, 373);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtuser);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "LogIn";
             this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ProyectoProgra04/ProyectoProgra04/Properties/Resources.Designer.cs
+++ ProyectoProgra04/ProyectoProgra04/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace ProyectoProgra04.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled {
+            get {
+                object obj = ResourceManager.GetObject("Untitled", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
