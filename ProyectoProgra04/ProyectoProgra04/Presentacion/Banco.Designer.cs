8e1bf3269f97611f785a35cc529f66ab1222b416
Tavo <gavc1@DESKTOP-IVNC8A5>
2017-11-26 21:05:44 -0600

    Commit

--- ProyectoProgra04/ProyectoProgra04/Presentacion/Banco.Designer.cs
+++ ProyectoProgra04/ProyectoProgra04/Presentacion/Banco.Designer.cs
@@ -28,36 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btncredito = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btncredito
-            // 
-            this.btncredito.Location = new System.Drawing.Point(16, 15);
-            this.btncredito.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btncredito.Name = "btncredito";
-            this.btncredito.Size = new System.Drawing.Size(151, 121);
-            this.btncredito.TabIndex = 0;
-            this.btncredito.Text = "Credito";
-            this.btncredito.UseVisualStyleBackColor = true;
-            this.btncredito.Click += new System.EventHandler(this.btncredito_Click);
-            // 
-            // Banco
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::ProyectoProgra04.Properties.Resources.Asuna01_30;
-            this.ClientSize = new System.Drawing.Size(428, 301);
-            this.Controls.Add(this.btncredito);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Banco";
             this.Text = "Banco";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btncredito;
     }
 }
