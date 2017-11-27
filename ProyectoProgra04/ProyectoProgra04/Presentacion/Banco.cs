ee76cf547d660d546c91fe2e6cb75167111d0f75
Tavo <gavc1@DESKTOP-IVNC8A5>
2017-11-26 18:59:33 -0600

    cambios

--- ProyectoProgra04/ProyectoProgra04/Presentacion/Banco.cs
+++ ProyectoProgra04/ProyectoProgra04/Presentacion/Banco.cs
@@ -66,15 +66,6 @@ namespace ProyectoProgra04.Presentacion
         {
         }
 
-        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
-        }
-
-        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
-        }
 
         private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
         {
