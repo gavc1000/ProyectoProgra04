8849686c04d53f0f30fe26f34368e413df0bd725
Jorge Isaac <jorgeisaacjimenezb@hotmail.com>
2017-12-02 22:12:16 -0600

    cliente

--- ProyectoProgra04/ProyectoProgra04/Presentacion/CreditoCliente.Designer.cs
+++ ProyectoProgra04/ProyectoProgra04/Presentacion/CreditoCliente.Designer.cs
@@ -29,20 +29,104 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreditoCliente));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.RadioButton();
+            this.aXml = new System.Windows.Forms.RadioButton();
+            this.Excel = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(291, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(418, 418);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(30, 194);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(97, 35);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.BackColor = System.Drawing.Color.Transparent;
+            this.txt.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt.ForeColor = System.Drawing.Color.White;
+            this.txt.Location = new System.Drawing.Point(27, 129);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(258, 31);
+            this.txt.TabIndex = 5;
+            this.txt.Text = "Exportar a Texto Plano";
+            this.txt.UseVisualStyleBackColor = false;
+            // 
+            // aXml
+            // 
+            this.aXml.AutoSize = true;
+            this.aXml.BackColor = System.Drawing.Color.Transparent;
+            this.aXml.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aXml.ForeColor = System.Drawing.Color.White;
+            this.aXml.Location = new System.Drawing.Point(30, 78);
+            this.aXml.Name = "aXml";
+            this.aXml.Size = new System.Drawing.Size(189, 32);
+            this.aXml.TabIndex = 4;
+            this.aXml.Text = "Exportar a XML";
+            this.aXml.UseVisualStyleBackColor = false;
+            // 
+            // Excel
+            // 
+            this.Excel.AutoSize = true;
+            this.Excel.BackColor = System.Drawing.Color.Transparent;
+            this.Excel.Checked = true;
+            this.Excel.FlatAppearance.BorderSize = 0;
+            this.Excel.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.Excel.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Excel.ForeColor = System.Drawing.Color.White;
+            this.Excel.Location = new System.Drawing.Point(30, 26);
+            this.Excel.Name = "Excel";
+            this.Excel.Size = new System.Drawing.Size(193, 31);
+            this.Excel.TabIndex = 3;
+            this.Excel.TabStop = true;
+            this.Excel.Text = "Exportar a Excel";
+            this.Excel.UseVisualStyleBackColor = false;
             // 
             // CreditoCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.BackgroundImage = global::ProyectoProgra04.Properties.Resources.Untitled;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(712, 424);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.aXml);
+            this.Controls.Add(this.Excel);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dataGridView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CreditoCliente";
-            this.Text = "CreditoCliente";
+            this.Text = "Gestion de Deducciones";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.RadioButton txt;
+        private System.Windows.Forms.RadioButton aXml;
+        private System.Windows.Forms.RadioButton Excel;
     }
 }
