042fcd8e7b6051963ccd9aee4542da43791eb55a
Tavo <gavc1@DESKTOP-IVNC8A5>
2017-12-02 23:42:36 -0600

    vcvxccscs

--- ProyectoProgra04/ProyectoProgra04/Presentacion/CreditoCliente.cs
+++ ProyectoProgra04/ProyectoProgra04/Presentacion/CreditoCliente.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ProyectoProgra04.Presentacion
 {
